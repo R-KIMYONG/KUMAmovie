6cceaf22d7a3eb1cc45485aa0d1f22a1c7e1df20
chasyuss <cotn963@gmail.com>
2024-05-07 12:55:06 +0900

    [feat]: add join local storage

--- js/join.js
+++ js/join.js
@@ -1,21 +1,28 @@
-function validateForm() {
-	var form = document.getElementById("signup-form");
-	var inputs = form.getElementsByTagName("input");
+function join() {
+	const getname = document.getElementsByClassName("name")[0];
+	const getemail = document.getElementsByClassName("email")[0];
+	const getpw = document.getElementsByClassName("password")[0];
+	const checkpw = document.getElementsByClassName("confirm_password")[0];
+	const signupButton = document.getElementsByClassName("signup-btn")[0];
 
-	// 모든 입력 필드가 채워져 있는지 확인
-	for (var i = 0; i < inputs.length; i++) {
-		if (inputs[i].value.trim() === "") {
-			alert("모든 필드를 채워주세요.");
-			return false;
-		}
-	}
+	signupButton.addEventListener("click", () => {
+		// event.preventDefault();
+		// console.log(getname.value);
+		// console.log(getemail.value);
+		// console.log(getpw.value);
+		// console.log(checkpw.value);
+		// localStorage.setItem("name", getname);
+		// localStorage.setItem("email", getemail);
+		// localStorage.setItem("password", getpw);
+		// localStorage.setItem("confirm_password", checkpw);
 
-	// 이용약관 동의 여부 확인
-	var agreeCheckbox = document.getElementById("checkbox");
-	if (!agreeCheckbox.checked) {
-		alert("이용약관에 동의해야 합니다.");
-		return false;
-	}
-
-	return true;
+		let joinInfo = {
+			name: getname.value,
+			email: getemail.value,
+			password: getpw.value,
+			confirm_password: checkpw.value
+		};
+		localStorage.setItem("joinInfo", JSON.stringify(joinInfo));
+	});
 }
+window.onload = join;
