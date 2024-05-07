8ba99cec5cd6ff90f74d27109fb5d4e710566671
chasyuss <cotn963@gmail.com>
2024-05-07 16:56:12 +0900

    [feat]: event js 추가

--- js/join.js
+++ js/join.js
@@ -9,6 +9,19 @@ function join() {
 		const password = document.getElementsByClassName("password")[0].value;
 		const confirmPassword = document.getElementsByClassName("confirm_password")[0].value;
 
+		if (!name || !id || !password || !confirmPassword) {
+			alert("빈칸없이 입력해주세요");
+			return;
+		}
+
+		const agreeCheckbox = document.getElementsByClassName("checkbox")[0].checked;
+
+		// 체크박스가 체크되지 않았을 때 경고 메시지 표시
+		if (!agreeCheckbox) {
+			alert("이용약관에 동의해야 회원가입이 가능합니다.");
+			return; // 제출 중지
+		}
+
 		// 기존 데이터의 수를 확인하여 새로운 키 생성
 		let userCount = 0;
 
@@ -47,6 +60,11 @@ function join() {
 	quitButton.addEventListener("click", () => {
 		const quitId = document.getElementsByClassName("quit-id")[0].value;
 
+		if (!quitId) {
+			alert("탈퇴할 ID를 입력하세요.");
+			return;
+		}
+
 		//같은 ID 찾고 삭제
 		for (let i = 0; i < localStorage.length; i++) {
 			const key = localStorage.key(i);
@@ -57,6 +75,7 @@ function join() {
 					console.log(`사용자 ${key}가 탈퇴되었습니다`);
 					alert(`${user.id}가 탈퇴되었습니다`);
 					location.reload();
+					break;
 				}
 			} else {
 				console.log("해당 ID의 사용자를 찾을 수 없습니다");
