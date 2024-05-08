2b6e455cd103131d33b5c7803a073bb3ef84609e
GA HYEON <dlrkxkszz@gmail.com>
2024-05-08 18:29:35 +0900

    5

--- js/login.js
+++ js/login.js
@@ -1,13 +1,5 @@
 "use strict";
 
-function login() {
-    var id = document.querySelector("#id");
-    var pw = document.querySelector("#pw");
-
-    if (id.value == "" || pw.value == "") {
-        alert("ID 또는 Password를 확인해주세요.");
-        return;
-    }
-    location.href = "index.html";
-    console.log(true)
+function login () {
+    
 }
